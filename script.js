9ccb5147ad5a0f110f289c5ca527d41b85d11952
myspace8 <comfyspace8@gmail.com>
2023-03-25 09:47:50 -0700

    fiexed ui

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1;
-const resetGameButton = document.getElementById('resetGame');
+const resetGameButton = document.getElementById('reset-game');
 const guesses = document.querySelector('.guesses');
-const lastResult = document.querySelector('.lastResult');
-const lowOrHi = document.querySelector('.lowOrHi');
-const guessSubmit = document.querySelector('.guessSubmit');
-const guessField = document.querySelector('.guessField');
+const lastResult = document.querySelector('.last-result');
+const lowOrHi = document.querySelector('.lowOrhigh');
+const guessSubmit = document.getElementById('guess-submit');
+const guessField = document.getElementById('guess-field');
 let guessCount = 1;
 let resetButton;
 
@@ -45,25 +45,24 @@ guessSubmit.addEventListener('click', checkGuess);
 function setGameOver() {
   guessField.disabled = true;
   guessSubmit.disabled = true;
-  // resetButton = document.createElement('button');
-  // resetButton.textContent = 'Start new game';
-  // document.phone.appendChild(resetButton);
   resetGameButton.style.display = 'block';
   resetGameButton.addEventListener('click', resetGame);
 }
 
 function resetGame() {
+  console.log('ddd');
   guessCount = 1;
   const resetParas = document.querySelectorAll('.resultParas p');
   for (const resetPara of resetParas) {
     resetPara.textContent = '';
   }
 
-  // resetButton.parentNode.removeChild(resetButton);
   guessField.disabled = false;
   guessSubmit.disabled = false;
   guessField.value = '';
   guessField.focus();
   lastResult.style.backgroundColor = 'white';
   randomNumber = Math.floor(Math.random() * 100) + 1;
+  resetGameButton.style.display = 'none'
+  guesses.textContent = ''
 }
